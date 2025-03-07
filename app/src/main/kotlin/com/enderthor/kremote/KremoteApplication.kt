5554070c7ea7b72ea1ec5c756f9851b10ebeade2
lockevod <58392928+lockevod@users.noreply.github.com>
2025-03-07 14:17:09 +0100

    Intent improvement

--- app/src/main/kotlin/com/enderthor/kremote/KremoteApplication.kt
+++ app/src/main/kotlin/com/enderthor/kremote/KremoteApplication.kt
@@ -58,8 +58,7 @@ class KremoteApplication : Application() {
 
     private fun startConnectionService() {
         try {
-            val intent = Intent(applicationContext, ConnectionServiceReceiver::class.java)
-            intent.action = "com.enderthor.kremote.START_CONNECTION_SERVICE"
+            val intent = Intent("com.enderthor.kremote.START_CONNECTION_SERVICE")
             intent.putExtra(ConnectionServiceReceiver.EXTRA_IS_EXTENSION, false)
             sendBroadcast(intent)
         } catch (e: Exception) {
