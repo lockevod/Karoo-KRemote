11bb42c394a9a471cfcd3caec9b7d58657156cba
Enderthor <58392928+lockevod@users.noreply.github.com>
2026-03-30 15:12:28 +0200

    Add Show Climber. Bumb extensions to 1.1.9. Remove firebase, Improve code

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.serialization) apply false
     alias(libs.plugins.jetbrains.kotlin.compose) apply false
-    alias(libs.plugins.google.dagger.hilt.android) apply false
-    alias(libs.plugins.google.devtools.ksp) apply false
+
 }
 
